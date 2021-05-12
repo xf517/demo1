1b8d5cdb9c7b3e744e64c9448859d30c8bed6a51
xufei <xufei0517>
2021-05-12 13:23:06 +0800

    zuul

--- zuul/src/main/java/com/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/zuul/ZuulApplication.java
@@ -2,6 +2,7 @@ package com.zuul;
 
 
 import com.fasterxml.jackson.databind.cfg.MapperConfig;
+import com.zuul.filter.LoggerFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +10,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @EnableDiscoveryClient
@@ -21,6 +23,11 @@ public class ZuulApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
         System.out.println("-------------------------ZuulApplication启动了");
+    }
+
+    @Bean
+    public LoggerFilter logFilter() {
+        return new LoggerFilter();
     }
 
 }
---
+++ zuul/src/main/java/com/zuul/filter/LoggerFilter.java
@@ -0,0 +1,32 @@
+package com.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Slf4j
+public class LoggerFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.error((String.format("send %s request to %s", request.getMethod(), request.getRequestURL().toString())));
+        return null;
+    }
+}
